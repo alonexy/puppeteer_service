c981a409b01904741afcefd06bdbd28afd4f5a90
alonexy <hpl1124@126.com>
2019-12-25 16:33:18 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -1,87 +1,71 @@
 var express = require('express');
-var puppeteer = require('puppeteer')
+var puppeteer = require('puppeteer');
 var router = express.Router();
-function init(){
-	(async () => {
-		for(var i=0;i<MAX_WSE;i++){
-			const browser = await puppeteer.launch({headless:true,
-				args: [
-				'--disable-gpu',
-				'--disable-dev-shm-usage',
-				'--disable-setuid-sandbox',
-				'--no-first-run',
-				'--no-sandbox',
-				'--no-zygote',
-				'--single-process'
-			]});
-			browserWSEndpoint = await browser.wsEndpoint();
-			WSE_LIST[i] = browserWSEndpoint;
-		}
-		console.log(WSE_LIST);
-	})();
+var bodyParser = require('body-parser');
+
+function init() {
+    (async () => {
+        for (var i = 0; i < MAX_WSE; i++) {
+            const browser = await puppeteer.launch({
+                headless: true,
+                args: [
+                    '--disable-gpu',
+                    '--disable-dev-shm-usage',
+                    '--disable-setuid-sandbox',
+                    '--no-first-run',
+                    '--no-sandbox',
+                    '--no-zygote',
+                    '--single-process'
+                ]
+            });
+            browserWSEndpoint = await browser.wsEndpoint();
+            WSE_LIST[i] = browserWSEndpoint;
+        }
+        console.log(WSE_LIST);
+    })();
 }
 
 const MAX_WSE = 2;  //启动几个浏览器
 let WSE_LIST = []; //存储browserWSEndpoint列表
 init();
-// 截屏
-router.get('/', function (req, res) {
-	let tmp = Math.floor(Math.random()* MAX_WSE);
-	(async () => {
-		let browserWSEndpoint = WSE_LIST[tmp];
-		const browser = await puppeteer.connect({browserWSEndpoint});
-		const page = await browser.newPage();
-		await page.goto('http://dev.crawl.local/api/v1/KlineView?symbol=XAUUSD&type=1&price=12356&rate=3.0&direction=down');
-		await page.setViewport({
-			 		width: 800,
-          height: 400
-		});
-		await page.waitForSelector("#main");
-		let body = await page.$('#main');
-        //调用页面内Dom对象的 screenshot 方法进行截图
-    let imgFile  = Date.parse( new Date() ).toString() + Math.ceil(Math.random()*100);
- 		await body.screenshot({
-            path: './public/images/'+imgFile+'.png'
-    });
-		await page.close();
-		res.send(imgFile);
-	})();
+//router.param('uri', function (req, res, next,uri) {
+//    next();
+//});
+router.get("/",function(req,res){
+    res.json({"msg":"ok","status":0});
+});
+router.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.json());
+// Test
+router.post('/test', function (req, res) {
+    let tmp = Math.floor(Math.random() * MAX_WSE);
+    (async () => {
+        try{
+            console.log("==>>>",req.body);
+            let uri = req.body.uri;
+            let regex = /^[http|https]+\:\/\/.*/;
+            if (!regex.test(uri)){
+                res.send("uri error");
+            }
+            let browserWSEndpoint = WSE_LIST[tmp];
+            const browser = await puppeteer.connect({browserWSEndpoint});
+            const page = await browser.newPage();
+            await page.goto(req.body.uri);
+            await page.waitForSelector("body");
+            let html = await page.content();
+            await page.close();
+            //res.json({"msg":"ok","data":html,"status":0});
+            res.send(html);
+        }catch(e){
+            res.send(e.toString());
+        }
+    })();
 });
 // 获取浏览器列表
 router.get('/browser_list', function (req, res) {
-	let tmp = Math.floor(Math.random()* MAX_WSE);
-	(async () => {
-		res.send(WSE_LIST);
-	})();
-});
-//爬虫测试
-router.post('/crawl', function (req, res) {
-	let tmp = Math.floor(Math.random()* MAX_WSE);
-	(async () => {
-		let browserWSEndpoint = WSE_LIST[tmp];
-		const browser = await puppeteer.connect({browserWSEndpoint});
-		const page = await browser.newPage();
-		await page.goto(req.body.url,{waitUntil: 'networkidle2'});
-		await page.addScriptTag({url: 'https://code.jquery.com/jquery-3.2.1.min.js'})
-		let consoles = []
-		page.on('console', msg => {
-		  for (let i = 0; i < msg.args().length; ++i)
-		    consoles.push(`${msg.args()[i]}`);
-		});
-		const result = await page.evaluate(code => {
-        try{
-					let retData = window.eval('(' + code + ')');
-					console.log(retData.load())
-	        return [true,retData.load()];
-        }catch(e){
-						return [false,e.toString()];
-        }
-    },req.body.js);
-    console.log()
-		await page.close();
-		res.json([consoles,result]);
-	})();
-
+    (async () => {
+        res.send(WSE_LIST);
+    })();
 });
 
 module.exports = router;
